26e5c5be6a0627708928f4829c0cfbcb22cf8d13
Juno Kim <lippoint.surf0622@gmail.com>
2021-09-29 10:43:57 +0900

    first commit

---
+++ StoreInfo.js
@@ -0,0 +1,214 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  Animated,
+} from 'react-native';
+import PagerView from 'react-native-pager-view';
+
+const BANNER_H = 335;
+
+const MyPager = () => {
+  return (
+    <PagerView style={styles.pagerView} initialPage={0}>
+      <View key="1">
+        <Text>First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+    </PagerView>
+  );
+};
+
+const StoreInfo = ({animatedValue}) => {
+  return (
+    <View>
+      <View style={styles.bannerContainer}>
+        <Animated.Image
+          source={require('./src/images/main01.jpg')}
+          style={styles.banner(animatedValue)}
+          resizeMode="cover"
+        />
+      </View>
+
+      <View
+        style={{
+          position: 'relative',
+          height: 220,
+        }}>
+        <View
+          style={{
+            position: 'absolute',
+            top: -35,
+            width: Dimensions.get('window').width - 40,
+            paddingVertical: 20,
+            borderRadius: 10,
+            backgroundColor: '#fff',
+            marginHorizontal: 20,
+            marginVertical: 20,
+            shadowColor: '#000',
+            shadowOffset: {width: 10, height: 10},
+            shadowOpacity: 1.0,
+            shadowRadius: 10,
+            elevation: 10,
+          }}>
+          <View
+            style={{
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignContent: 'center',
+            }}>
+            <Text style={{textAlign: 'center', fontSize: 25, marginBottom: 15}}>
+              집밥한끼
+            </Text>
+            <View
+              style={{
+                alignSelf: 'center',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: 10,
+              }}>
+              <Image
+                source={require('./src/images/thumbup.png')}
+                style={{width: 35, height: 35}}
+                resizeMode="cover"
+              />
+              <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                <Text style={{fontSize: 18, fontWeight: 'bold'}}>맛</Text>이
+                최고에요!
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: 20,
+              }}>
+              <Text style={{textAlign: 'center', fontSize: 14, color: '#666'}}>
+                최근리뷰 35
+              </Text>
+              <View
+                style={{
+                  height: '65%',
+                  width: 1,
+                  backgroundColor: '#666',
+                  marginHorizontal: 10,
+                }}
+              />
+              <Text style={{textAlign: 'center', fontSize: 14, color: '#666'}}>
+                최근사장님댓글 34
+              </Text>
+            </View>
+            <View
+              style={{
+                paddingHorizontal: 20,
+                marginTop: 10,
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('./src/images/phone-call.png')}
+                  style={{width: 15, height: 15, marginRight: 5}}
+                  resizeMode="contain"
+                />
+                <Text style={{fontSize: 15}}>전화</Text>
+              </TouchableOpacity>
+              <View
+                style={{width: 1, height: '65%', backgroundColor: '#e6e6e6'}}
+              />
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('./src/images/heart.png')}
+                  style={{width: 15, height: 15, marginRight: 5}}
+                  resizeMode="contain"
+                />
+                <Text style={{fontSize: 15}}>찜 275</Text>
+              </TouchableOpacity>
+              <View
+                style={{width: 1, height: '65%', backgroundColor: '#e6e6e6'}}
+              />
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('./src/images/share.png')}
+                  style={{width: 15, height: 15, marginRight: 5}}
+                  resizeMode="contain"
+                />
+                <Text style={{fontSize: 15}}>공유</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+      {/* 쿠폰 */}
+      <View style={{paddingHorizontal: 20, marginBottom: 20}}>
+        <Image
+          source={require('./src/images/visual_welcome.jpg')}
+          style={{width: '100%', height: 80}}
+          resizeMode="contain"
+        />
+      </View>
+      {/* //쿠폰 */}
+      {/* 가게 정보 탭 */}
+      <MyPager />
+      {/* //가게 정보 탭 */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pagerView: {
+    flex: 1,
+    height: 200,
+  },
+  bannerContainer: {
+    marginTop: -1000,
+    paddingTop: 1000,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  banner: animatedValue => ({
+    height: BANNER_H,
+    width: '100%',
+    transform: [
+      {
+        translateY: animatedValue.interpolate({
+          inputRange: [-BANNER_H, 0, BANNER_H, BANNER_H + 1],
+          outputRange: [-BANNER_H / 2, 0, BANNER_H * 0.75, BANNER_H * 0.75],
+        }),
+      },
+      {
+        scale: animatedValue.interpolate({
+          inputRange: [-BANNER_H, 0, BANNER_H, BANNER_H + 1],
+          outputRange: [2, 1, 1, 0.5],
+        }),
+      },
+    ],
+  }),
+});
+
+export default StoreInfo;
---
+++ data/index.js
@@ -0,0 +1,195 @@
+export const navDate = [
+  {
+    id: 0,
+    name: '세트류',
+  },
+  {
+    id: 1,
+    name: '밥류',
+  },
+  {
+    id: 2,
+    name: '면류',
+  },
+  {
+    id: 3,
+    name: '튀김류',
+  },
+  {
+    id: 4,
+    name: '야식류',
+  },
+  {
+    id: 5,
+    name: '일식',
+  },
+  {
+    id: 6,
+    name: '중식',
+  },
+  {
+    id: 7,
+    name: '한식',
+  },
+  {
+    id: 8,
+    name: '1인분',
+  },
+];
+
+export const menus = [
+  {
+    id: 0,
+    name: '세트류',
+    details: [
+      {
+        name: '탕수육세트',
+        price: '15,000',
+        img: 'https://img-cf.kurly.com/shop/data/goodsview/20200417/gv10000091018_1.jpg',
+      },
+      {
+        name: '양장피세트',
+        price: '17,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕세트',
+        price: '12,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: '밥류',
+    details: [
+      {
+        name: '짜장밥',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕밥',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '볶음밥',
+        price: '5,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '면류',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: '튀김류',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: '야식류',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: '일식',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: '중식',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: '한식',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: '1인분',
+    details: [
+      {
+        name: '짜장면',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+      {
+        name: '짬뽕',
+        price: '6,000',
+        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+      },
+    ],
+  },
+];
