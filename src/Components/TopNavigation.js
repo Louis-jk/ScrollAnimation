c3b2c22d04af135afb0e8ae7d319d66b963fc833
Juno Kim <lippoint.surf0622@gmail.com>
2021-09-29 13:42:24 +0900

    상단 네비게이션 생성

---
+++ src/Components/TopNavigation.js
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {BANNER_H, TOPNAVI_H} from '../constans';
+
+const TopNavigation = props => {
+  const {title, navigation, animatedValue} = props;
+  const isFloating = !!animatedValue;
+  const [isTransparent, setTransparent] = useState(isFloating);
+  const safeArea = useSafeAreaInsets();
+
+  console.log('animatedValue', animatedValue);
+  console.log('safeArea', safeArea);
+  console.log('isFloating', isFloating);
+
+  useEffect(() => {
+    if (!animatedValue) {
+      return;
+    }
+    const listenerId = animatedValue.addListener(a => {
+      const topNaviOffset = BANNER_H - TOPNAVI_H - safeArea.top;
+      isTransparent !== a.value < topNaviOffset &&
+        setTransparent(!isTransparent);
+    });
+    return () => animatedValue.removeListener(listenerId);
+  });
+
+  return (
+    <>
+      <StatusBar
+        barStyle={isTransparent ? 'light-content' : 'dark-content'}
+        translucent
+        backgroundColor="transparent"
+      />
+      <View style={styles.container(safeArea, isFloating, isTransparent)}>
+        <TouchableOpacity>
+          <Image
+            source={
+              isTransparent
+                ? require('../images/top_back_wh.png')
+                : require('../images/top_back.png')
+            }
+            style={{width: 30, height: 25}}
+            resizeMode="cover"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={
+              isTransparent
+                ? require('../images/ico_search_wh.png')
+                : require('../images/ico_search.png')
+            }
+            style={{width: 30, height: 25}}
+            resizeMode="cover"
+          />
+        </TouchableOpacity>
+        {/* <Text style={styles.title(isTransparent)}>{title}</Text> */}
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: (safeArea, isFloating, isTransparent) => ({
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItem: 'center',
+    paddingTop: safeArea.top,
+    marginBottom: isFloating ? -TOPNAVI_H - safeArea.top : 0,
+    height: TOPNAVI_H + safeArea.top,
+    shadowOffset: {y: 0},
+    backgroundColor: isTransparent ? '#0001' : '#FFF',
+    shadowOpacity: isTransparent ? 0 : 0.5,
+    elevation: isTransparent ? 0.01 : 5,
+    zIndex: 100,
+  }),
+  title: isTransparent => ({
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: isTransparent ? '#FFF' : '#000',
+  }),
+});
+
+export default TopNavigation;
