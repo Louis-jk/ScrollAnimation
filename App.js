23bafe63339035663e336c94427162b60bf36076
Juno Kim <lippoint.surf0622@gmail.com>
2021-09-30 15:59:03 +0900

    redux provider wrapping

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
+import {Provider} from 'react-redux';
+import initStore from './src/redux/store';
 import StoreDetail from './src/Screens/StoreDetail';
+
+const store = initStore();
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <StoreDetail />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <StoreDetail />
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
