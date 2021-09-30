bd94e58f322e690ff9878201da4ac47e5643751b
Juno Kim <lippoint.surf0622@gmail.com>
2021-09-30 15:59:51 +0900

    메뉴 정보 설명문 추가

--- data/index.js
+++ data/index.js
@@ -44,25 +44,39 @@ export const menus = [
     name: '세트류',
     details: [
       {
-        name: '[오리지날 SET]탕수육세트',
+        name: '[계절을느끼자!]오리고기부추무침&된장찌게된장찌게된장찌게',
         description:
           '참 맛있는 요리입니다. 달콤 짭잘한 소스가 더해진 일품 요리입니다. 참 맛있어요.',
         price: '15,000',
-        img: 'https://img-cf.kurly.com/shop/data/goodsview/20200417/gv10000091018_1.jpg',
+        img: 'https://i.pinimg.com/originals/c4/ca/24/c4ca24ea9bb19c7dd6f44aaf0a636cb4.jpg',
       },
       {
-        name: '양장피세트',
+        name: '[계절밥상 소반]갓지은 솔밥과 즉석에서 구워내는 직화구이까지!',
+        description:
+          '참 맛있는 요리입니다. 달콤 짭잘한 소스가 더해진 일품 요리입니다. 참 맛있어요.',
+        price: '15,000',
+        img: 'https://www.cjfoodworld.com:7015/UploadFiles/Brend/20170914132952313945.jpg',
+      },
+      {
+        name: '[따뜻한 기분]시레기국&오뎅세트',
+        description:
+          '참 맛있는 요리입니다. 달콤 짭잘한 소스가 더해진 일품 요리입니다. 참 맛있어요.',
+        price: '15,000',
+        img: 'https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/dRD/image/uoVA0691SUO_zoj4TPauYFPrhKc.png',
+      },
+      {
+        name: '[화끈화끈!달끈달끈!]순두부찌게세트',
         description:
           '참 맛있는 요리입니다. 달콤 짭잘한 소스가 더해진 일품 요리입니다. 참 맛있어요.',
         price: '17,000',
-        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+        img: 'https://lh3.googleusercontent.com/proxy/nz0qtprsIz7avKBw6-dsqlhR744v-77iAJMsfCchHn6gMcZxR9N_iDzvZ0p3qos8maDnCPAg3a58un0gMvcv796yaayfxcBTKW-JRTWYDPihSCTI',
       },
       {
-        name: '짬뽕세트',
+        name: '[집밥이 좋다]된장국세트',
         description:
           '참 맛있는 요리입니다. 달콤 짭잘한 소스가 더해진 일품 요리입니다. 참 맛있어요.',
         price: '12,000',
-        img: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F253ECC4A5617933305',
+        img: 'https://lh3.googleusercontent.com/proxy/qxSHas0OWRIBtUZte14AP4qH-ApcjYJndTL5cO--RgCzMIfslfpNtpQ072gDiRuzzHpR02Y-sGuXFyNhU51cz7ebz8JFIjW76IW_2DvAR3esgA',
       },
     ],
   },
